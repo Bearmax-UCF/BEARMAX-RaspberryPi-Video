a8c80e57eaa3c2b0575e4cece3fa46849fe1f303
Sabrina-Lopez <sa568892@ucf.edu>
2024-03-13 01:49:09 -0400

    added some client-side socket code based on the server-side socket code and the robot control socket code

--- SocketIoClient.py
+++ SocketIoClient.py
@@ -25,8 +25,42 @@ class SocketIoClient:
             await self.socketioclient.connect(DEFAULT_URL, headers={'Authorization': token})
             print('connection established')
 
-        @sio.event
+        @self.socketioclient.event
         async def connect_error():
             print("The connection failed!")
+        
+        @self.socketioclient.event
+        async def disconnect():
+            self.logger.info('Disconnected from WebSocket Server')
+
+        @self.socketioclient.event
+        async def disconnecting():
+            self.logger.info('Disconnecting from WebSocket Server')
+
+        @self.socketioclient.event
+        async def speak(msg):
+            self.publisher_.publish(self.to_msg(f"ACK: [{msg}]"))
+        
+        @self.sio_.event
+        async def ping():
+            self.logger.info("Pong!")
+
+        @self.sio_.event
+        async def sensoryOverloadAid(action, userID):
+            self.logger.info(f"Got aid state command: {action} | {userID}")
+            if not action in ("start", "stop"):
+                self.logger.error(
+                    f"sensoryOverloadAid handler expected string action and got {action}")
+            # "aidStart" or "aidStop"
+            self.publisher_.publish(self.to_msg(
+                "aid" + action.capitalize() + "-" + userID))
+
+        @self.sio_.event
+        async def playMedia(mediaURL):
+            self.logger.info(f"Got string for media URL: {mediaURL}")
+            if not mediaURL.endswith(".mp3") and not mediaURL.endswith(".mp4"):
+                self.logger.error("Invalid media file format")
+            self.publisher_.publish(self.to_msg(
+                "playMedia-" + mediaURL))
 
     # Once connection is established, we want to listen for the event
