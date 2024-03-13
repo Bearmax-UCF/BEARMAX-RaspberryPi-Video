c445f6e8586c532176623ed5a4f370bdf0571168
Jason Lin <jasonlinfl@gmail.com>
2024-03-13 11:44:48 -0400

    update VideoDisplay to be able to display multiple videos

--- videoDisplay.py
+++ videoDisplay.py
@@ -6,26 +6,29 @@ class VideoDisplay:
     def __init__(self, videoLink: str):
         self.videoLink = videoLink
         self.video = cv2.VideoCapture(videoLink)
-    def displayVideo(self) -> bool:
+    def displayVideo(self):
         cap = cv2.VideoCapture(self.videoLink)
         cv2.namedWindow('Frame', cv2.WND_PROP_FULLSCREEN)
         cv2.setWindowProperty('Frame', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
         while cap.isOpened():
             ret, frame = cap.read()
-            if ret == True:
+            
+            if ret:
                 cv2.imshow('Frame', frame)
-                if cv2.waitKey(25) & 0xFF == ord('q'):
-                    return False
+
             else:
-                return False
+                break
+
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
         cap.release()
         cv2.destroyAllWindows()
-        return True
+        cv2.waitKey(1)
 test="https://bearmaxtest.blob.core.windows.net/test/Metal_pipe_falling_sound_effectloud.mp4?sp=r&st=2024-03-13T05:10:29Z&se=2024-03-13T13:10:29Z&spr=https&sv=2022-11-02&sr=b&sig=V9rSsSetFc%2BhirlWk8CjJu8KpAgQZDCN%2BJGUMEpzkEU%3D"
 # Example usage
 def main():        
     inputVideo= input("Enter the video link: ")
     video = VideoDisplay(inputVideo)
     video.displayVideo()
-
 main()
